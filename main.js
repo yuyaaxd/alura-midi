3d3b9877610d703c0e590671ff87633de7b05718
Junior <128407456+yuyaaxd@users.noreply.github.com>
2023-11-14 15:57:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ function tocaSom (seletorAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
