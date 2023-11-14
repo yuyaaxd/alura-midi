a38bd27541f5b3675546eec1164f6e859c082f6b
Junior <128407456+yuyaaxd@users.noreply.github.com>
2023-11-14 15:56:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
 function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
-    if (elemento && elemento.localname === 'audio' ) {
+    if (elemento && elemento.localName === 'audio') {
         elemento.play();
     }
     else {
-        console.log('Elemento não encontrado ou seletor inválido.');
+        console.log('Elemento não encontrado ou seletor inválido');
     }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; 
 
-    listaDeTeclas[contador].onclick = function () {
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
     tecla.onkeydown = function (evento) {
-         if (evento.code === 'Space' || evento.code === 'Enter' ) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
- }
+
+    }
 
     tecla.onkeyup = function () {
-        tecla.classList.remove ('ativa');
+        tecla.classList.remove('ativa');
     }
 
 }
